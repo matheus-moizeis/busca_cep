1cc3a48fc43fcb84afa20179d9e3ebd4401420f5
Matheus Moizeis <matheusoliveira.m98@gmail.com>
2020-09-05 02:56:13 -0300

    Configuração componente Register

--- frontend/src/screens/Nav/index.jsx
+++ frontend/src/screens/Nav/index.jsx
@@ -5,17 +5,17 @@ export default (props) => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link to='/register' className="nav-link"> Cadastros </Link>
-                    </li>
-  
-                    <li className="nav-item active">
-                        <Link to='/consult' className="nav-link"> Cadastros </Link>
-                    </li>
-                </ul>
+                <div className="collapse navbar-collapse justify-content-md-center">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link to='/register' className="nav-link"> Cadastros </Link>
+                        </li>
 
-
+                        <li className="nav-item active">
+                            <Link to='/consult' className="nav-link"> Cadastros </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
--- frontend/src/screens/Register/index.jsx
+++ frontend/src/screens/Register/index.jsx
@@ -1,60 +1,76 @@
 import React from 'react';
-import './style.css'
 
 export default (props) => {
     return (
-        <div className='container h-100 pt-5'>
-            <h1>Cadastrar</h1>
-            <div className="d-flex flex-column h-100">
-                <form>
-                    <legend>Dados Pessoais</legend>
-                    <div className="form-group">
-                        <label >Cpf</label>
-                        <input type="number" className="form-control input-form" />
-                    </div>
+        <div className="container">
 
-                    <div className="form-group">
-                        <label >Nome</label>
-                        <input type="text" className="form-control input-form" />
-                    </div>
-
-                    <legend>Dados Pessoais</legend>
-                    <div className="form-group">
-                        <label >CEP</label>
-                        <input type="number" className="form-control input-form" />
-                    </div>
-
-                    <div className="form-group">
-                        <label >Rua</label>
+            <form className="form-horizontal">
+                <legend >Dados Pessoais</legend>
+                <div className="form-group ">
+                    <label className="control-label col-sm-2">Nome:</label>
+                    <div className="col-sm-3">
                         <input type="text" className="form-control" />
                     </div>
+                </div>
 
-                    <div className="form-group">
-                        <label >Número</label>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-2">CPF:</label>
+                    <div className="col-sm-2">
                         <input type="number" className="form-control" />
                     </div>
+                </div>
 
-                    <div className="form-group">
-                        <label >Bairro</label>
+                <legend>Endereço</legend>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-2">CEP:</label>
+                    <div className="col-sm-2">
+                        <input type="number" className="form-control" />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-2">Rua:</label>
+                    <div className="col-sm-4">
                         <input type="text" className="form-control" />
                     </div>
+                </div>
 
-                    <div className="form-group">
-                        <label >Cidade</label>
+                <div className="form-group">
+                    <label className="control-label col-sm-2">Número:</label>
+                    <div className="col-sm-2">
+                        <input type="number" className="form-control" />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-2">Bairro:</label>
+                    <div className="col-sm-3">
                         <input type="text" className="form-control" />
                     </div>
+                </div>
 
-
-                    <div className="form-group">
-                        <label >Estado</label>
+                <div className="form-group">
+                    <label className="control-label col-sm-2">Cidade:</label>
+                    <div className="col-sm-4">
                         <input type="text" className="form-control" />
                     </div>
+                </div>
 
-                    <div>
-                        <button className="btn btn-primary btn-round">Cadastrar</button>
+                <div className="form-group">
+                    <label className="control-label col-sm-2">Estado:</label>
+                    <div className="col-sm-1">
+                        <input type="txt" className="form-control" />
                     </div>
-                </form>
-            </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-sm-offset-2 col-sm-10">
+                        <button type="submit" className="btn btn-primary">Salvar</button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
+
